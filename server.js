9adeced180f6a686ac085e15fb8c6373e33812cd
RAM-BHARATH <ramtherapid007@gmail.com>
2022-09-26 12:36:44 +0530

    Added headers netlify.toml

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.use(express.urlencoded({ extended:false }))
     
 // }))
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://goalsetter-frontend.netlify.app'
+}))
 
 app.use('/api/goals', goal_routes);
 app.use('/api/users', user_routes);
